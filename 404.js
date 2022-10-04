56b534283535ad9598c78ba299cc0af464bda45e
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-10-04 16:30:47 +0700

    add error page

---
+++ 404.js
@@ -0,0 +1,7 @@
+const errorPage = () => (
+  <div className="min-h-screen w-full justify-center items-center">
+    <h1 className="font-bold text-center text-black">404 Page Not Found</h1>
+  </div>
+);
+
+export default errorPage;
