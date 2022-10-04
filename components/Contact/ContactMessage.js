b7c1d2a0eb1d23e062e41753f3588b7d18a32626
michael leopold tinangon <michaelleopold2016@gmail.com>
2022-10-04 10:43:40 +0700

    add loading & message status when sending mail

--- components/Contact/ContactMessage.js
+++ components/Contact/ContactMessage.js
@@ -1,18 +1,27 @@
 /* eslint-disable no-alert */
-import { memo, useRef } from 'react';
+import { memo, useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { Player } from '@lottiefiles/react-lottie-player';
+import loadingLottie from '../../public/lotties/general-loading.json';
 
 const ContactMessage = () => {
   const form = useRef();
+
+  const [loading, setLoading] = useState(false);
+  const [successOrFailMessage, setSuccessOrFailMessage] = useState('');
 
   const onSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const result = await emailjs.sendForm('service_vmn3enh', 'template_m4eedfq', form.current, 'gqFXv52IsbUQO8y5G');
-      console.log('result=', result);
+      setSuccessOrFailMessage('');
+      setLoading(true);
+      await emailjs.sendForm('service_vmn3enh', 'template_m4eedfq', form.current, 'gqFXv52IsbUQO8y5G');
+      setLoading(false);
+      setSuccessOrFailMessage('true');
     } catch (err) {
-      console.log('error=', err);
+      setLoading(false);
+      setSuccessOrFailMessage('fail');
     }
   };
 
@@ -34,7 +43,19 @@ const ContactMessage = () => {
             <input placeholder="Subject" required name='subject' className="flex flex-1 h-[2.75rem] pl-3 items-center rounded-md shadow-sm overflow-hidden border-[0.12rem] border-gray-300 text-gray-900 text-sm" />
           </div>
           <textarea placeholder="Message" required name='message' className="w-full h-32 mt-3 flex rounded-md shadow-sm overflow-hidden border-[0.12rem] border-gray-300 text-gray-900 text-sm p-3 md:mt-3" />
-          <input type="submit" value="Send Message" className="w-32 md:w-36 h-10 md:h-12 mt-3 flex justify-center items-center text-white font-bold bg-[#0094DF] rounded-md shadow-md hover:opacity-70 active:opacity-30 cursor-pointer md:mt-4" />
+          {loading ? (
+            <div className="w-32 md:w-36 h-10 md:h-12 mt-3 flex justify-center items-center bg-gray-200 rounded-md shadow-md md:mt-4">
+              <Player
+                autoplay
+                loop
+                src={loadingLottie}
+                className="h-20 w-20"
+              />
+            </div>
+          ) : (
+            <input type="submit" value="Send Message" className="w-32 md:w-36 h-10 md:h-12 mt-3 flex justify-center items-center text-white font-bold bg-[#0094DF] rounded-md shadow-md hover:opacity-70 active:opacity-30 cursor-pointer md:mt-4" />
+          )}
+          <h1 className={`${successOrFailMessage === '' ? 'hidden' : 'block'} ${successOrFailMessage === 'fail' ? 'text-red-500' : 'text-[#0094DF]'}`}>{successOrFailMessage === 'fail' ? 'Failed sending mail, please try again' : 'Success sending mail'}</h1>
         </form>
       </div>
     </div>
